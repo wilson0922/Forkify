4059df50995187ba29efa4e83c06df5449e007e9
tree70373 <tree70373@gmail.com>
2023-05-31 09:56:14 +0800

    init

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/'
+export const TIMEOUT_SEC = 10
+export const RES_PER_PAGE = 10
+export const KEY = '3760ec0d-8838-4e59-b61a-a11378be578e'
+export const MODEL_CLOSE_SEC = 2.5
